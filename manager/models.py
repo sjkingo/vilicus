e5bbfd9ff9a4d78d84268819d80bbddb1595e009
Sam Kingston <sam@sjkwi.com.au>
2013-11-20 05:31:14 +1000

    add Meta class for Agent

--- manager/models.py
+++ manager/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Agent(models.Model):
     hostname = models.CharField(max_length=100)
     last_checkin = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Agent'
+        verbose_name_plural = verbose_name + 's'
+
+    def __unicode__(self):
+        return self.hostname
