593abdef831bae9164e3ff16d434e2cc5e4314f8
Sam Kingston <sam@sjkwi.com.au>
2013-11-23 06:39:06 +1000

    fixed navbar

--- vilicus/settings.py
+++ vilicus/settings.py
@@ -107,6 +107,17 @@ INSTALLED_APPS = (
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'dashboard.context_processors.site',
+)
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
